29484d02f98eac2eed41d2cecae614e7f3963e37
Siva Sathvik <sathvikv04@gmail.com>
2026-09-22 16:28:29 +0530

    feat(export): add CSV export utility for exam results

---
+++ export.py
@@ -0,0 +1,5 @@
+﻿def export_results_to_csv(results, filepath):
+    with open(filepath, 'w') as f:
+        f.write('student,score,status\n')
+        for r in results:
+            f.write(f\"{r['name']},{r['score']},{r['passed']}\n\")
